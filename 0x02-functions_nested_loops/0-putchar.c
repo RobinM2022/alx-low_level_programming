a79c0115a5fa3aaff6d5c3bacaa2d7f75c9f5e52
RobinM2022 <robin.mbondo@gmail.com>
2022-10-21 08:33:39 +0000

    Added a README file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+  * main - prints _putchar, followed by a new line
+  *
+  * Return: 0 always when successful
+  */
+int _putchar(char _putchar)
+{
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+File: 0-putchar.c has a program that prints _putchar, followed by a new line.
+
+
