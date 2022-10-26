b08f3e42410c206d83801b8006fe92aaf1bdb9b5
RobinM2022 <robin.mbondo@gmail.com>
2022-10-26 14:50:15 +0000

    Ammended task 1 to fit Betty coding style

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-  * reset_to_98 - takes a point to an int and updates its value to 98
-  * @n: pointer to int
-  *
-  */ 
+ * reset_to_98 - takes a point to an int and updates its value to 98
+ * @n: pointer to int
+ *
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;
