f24edb83cb637ca9d8cde87023f0d8c77b96ce54
RobinM2022 <robin.mbondo@gmail.com>
2022-10-26 15:56:59 +0000

    Added the answer for task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: The string to get the length of.
+ *
+ * Return: The length of @s.
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	return (count);
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -2,7 +2,7 @@ File 0-reset_to_98.c has a function that takes a pointer to an int as parameter 
 
 File 1-swap.c has a function that swaps the values of two integers.
 
-File
+File 2-strlen.c has a function that returns the length of a string. 
 
 File
 
