0e9a0f16f20307f1cac2d8140ca96385492317fe
RobinM2022 <robin.mbondo@gmail.com>
2022-11-07 14:45:37 +0000

    Added the code for task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - prints the number of arguments passed into it
+  * @argc: the number of command line arguments
+  * @argv: pointer to array of strings passed to the command line
+  * Return: 0 when sucessful.
+  */
+
+int main(int argc, char *argv[] __attribute__((unused)))  
+{
+	printf("The number received by argc is %d:\n", argc - 1);
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,3 +1,5 @@
 File 0-whatsmyname.c has a program that prints its name, followed by a new line.
 
+File 1-args.c has a program that prints the number of arguments passed into it, followed by a new line.
+
 
