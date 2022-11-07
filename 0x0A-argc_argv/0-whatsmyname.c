8f1a759fe80dc2b3f03d3a1294ac0bd8f5f94502
RobinM2022 <robin.mbondo@gmail.com>
2022-11-07 16:25:00 +0000

    Ammended the code for task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,8 @@
   * Return: 0 when successful.
   */
 
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc __attribute__((__unused__)), char *argv[])
 {
 	printf("%s.\n", argv[0]);
-	
 	return (0);
 }
