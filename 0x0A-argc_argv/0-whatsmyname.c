5f5fbebaefad91024d201ed9100a5be563124299
RobinM2022 <robin.mbondo@gmail.com>
2022-11-07 16:17:50 +0000

    Changed the code for task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
 /**
-  * main - prints the program name, folloed by a new line
+  * main - prints the program name, followed by a new line
   * @argc: the number of arguments passed to the command line
   * @argv: pointer to an array of char pointers
   *
   * Return: 0 when successful.
   */
 
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	(void)argc;
-
 	printf("%s.\n", argv[0]);
+	
 	return (0);
 }
