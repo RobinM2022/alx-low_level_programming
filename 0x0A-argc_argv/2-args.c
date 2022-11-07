c79c10eb63694b3982568b16009ca7b515786f4f
RobinM2022 <robin.mbondo@gmail.com>
2022-11-07 16:47:37 +0000

    Added the code for task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -11,12 +11,7 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	if (argc)
-	{
-		printf("%s\n", argv[0]);
-		printf("\n");
-	}
-	for (i = 1; i < argc; i++)
+	for (i = 0; i < argc; i++)
 		printf("%s\n", argv[i]);
 	return (0);
 }
