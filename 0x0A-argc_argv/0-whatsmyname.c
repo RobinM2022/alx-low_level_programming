630807f82a411610828503792e0d524cb33e2c17
RobinM2022 <robin.mbondo@gmail.com>
2022-11-07 14:22:17 +0000

    Added the code for task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - prints the program name, folloed by a new line
+  * @argc: the number of arguments passed to the command line
+  * @argv: pointer to an array of char pointers
+  * Return: 0 when successful.
+  */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s.\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,3 @@
+File 0-whatsmyname.c has a program that prints its name, followed by a new line.
+
+
