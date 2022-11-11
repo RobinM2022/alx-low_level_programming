faa19549da345c148bd24f8a31222e4588e2ac4d
RobinM2022 <robin.mbondo@gmail.com>
2022-11-11 12:44:01 +0000

    Added the answer for task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * _strdup - Returns a pointer to a newly allocated space in memory,
+  * containing a copy of the string given as a parameter.
+  * @str: The string to be copied.
+  * 
+  * Return: NULL if str == NULL or insufficient memory is available
+  *	    Otherwise, a pointer to the duplicated string.
+  */
+char *_strdup(char *str)
+{
+	char *a;
+	int i, j = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		j++;
+
+	a = malloc(sizeof(char) * (j + 1));
+
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; str[i]; i++)
+		a[i] = str[i];
+
+	a[j] = '\0';
+
+	return (a);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,3 +1,7 @@
-	File 0-create_array.c has a function that creates an array of chars, and initializes it with a specific char		acter.
+	File 0-create_array.c has a function that creates an array of chars, and initializes it with a specific character.
+
+	File 1-strdup.c has a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.
+
+
 	
 
