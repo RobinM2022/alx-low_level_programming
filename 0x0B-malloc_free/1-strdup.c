0c225f3e33db6041e8c75959d88f71249487123d
RobinM2022 <robin.mbondo@gmail.com>
2022-11-11 12:46:21 +0000

    Adjusted the code to accomdate Betty style

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -5,7 +5,6 @@
   * _strdup - Returns a pointer to a newly allocated space in memory,
   * containing a copy of the string given as a parameter.
   * @str: The string to be copied.
-  * 
   * Return: NULL if str == NULL or insufficient memory is available
   *	    Otherwise, a pointer to the duplicated string.
   */
