c2e12a35d27d4463ed035bb9efe56a3f390f9a7f
RobinM2022 <robin.mbondo@gmail.com>
2022-11-22 13:43:28 +0000

    Added the code for task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+  * print_numbers - prints numbers, followed by a newline.
+  * @n: The number of integers passed to the function.
+  * @separator: The string to be printed.
+  *
+  * Return: Nothing
+  */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	if (separator == NULL)
+		return;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, const unsigned int));
+
+		if (i != (n - 1))
+		printf("%s", separator);
+	}
+	printf("\n");
+
+	va_end(args);
+}
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -2,4 +2,6 @@
 
 * 0-sum_them_all.c
 
-* 
+* 1-print_numbers.c
+
+
