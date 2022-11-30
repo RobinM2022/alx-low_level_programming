0af59016f7f25fc66dcc2d52f40331b9aa89f9b3
RobinM2022 <robin.mbondo@gmail.com>
2022-11-30 08:46:22 +0000

    Ammended the code to fit in with Betty coding style

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
-  * list_len - finds the number of elements in a linked list. 
+  * list_len - finds the number of elements in a linked list.
   * @h: The linked list (list_t).
   *
   * Return: The number of elements in h.
