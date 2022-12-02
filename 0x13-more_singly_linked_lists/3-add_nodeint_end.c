3ea77a6b72a02e5a8d80f7dc2f87a581da53e2d9
RobinM2022 <robin.mbondo@gmail.com>
2022-12-02 09:11:45 +0000

    Adjusted the code to fit in with Betty coding style

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
 /**
-  * add_nodeint_end - Adds a node to the end of a listint_t list.
-  * @head: A pointer to the head of the listint_t list.
-  * @n: The number of elements in the list.
-  *
-  * Return: NULL if function fails.
-  *         Otherwise, address of the new element. 
-  */
+ * add_nodeint_end - Adds a node to the end of a listint_t list.
+ * @head: A pointer to the head of the listint_t list.
+ * @n: The number of elements in the list.
+ *
+ * Return: NULL if function fails.
+ *         Otherwise, address of the new element.
+ */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new;
@@ -27,9 +27,9 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	else
 	{
 		last = *head;
-		while (last-> next != NULL)
-			last = last -> next;
-		last-> next = new;
+		while (last->next != NULL)
+			last = last->next;
+		last->next = new;
 	}
 
 	return (*head);
