5e18ff5bbd1e38c06ecb0577bf0a688add31ec51
RobinM2022 <robin.mbondo@gmail.com>
2022-12-02 13:12:14 +0000

    Added two lines to take care of NULL return

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,6 +14,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	for (node = 0; node < index; node++)
 	{
+		if (head == NULL)
+		return (NULL);
+
 		head = head->next;
 	}
 
