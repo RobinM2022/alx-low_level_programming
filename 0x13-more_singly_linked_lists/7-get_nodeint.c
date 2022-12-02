19b7113670d5f609eaa34fcbf0df32a080290f33
RobinM2022 <robin.mbondo@gmail.com>
2022-12-02 13:08:24 +0000

    Adjusted the code to fit in with Betty coding style

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
 /**
-  * get_nodeint_at_index - Locates the nth node of a linked listint_t list.
-  * @head: A pointer to the head of the list.
-  * @index: The node to locate.
-  *
-  * Return: nth node of the list.
-  *         NuLL if the node is empty. 
-  */
+ * get_nodeint_at_index - Locates the nth node of a linked listint_t list.
+ * @head: A pointer to the head of the list.
+ * @index: The node to locate.
+ *
+ * Return: nth node of the list.
+ *         NuLL if the node is empty.
+ */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int node;
