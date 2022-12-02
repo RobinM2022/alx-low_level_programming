3abd3981764e79b41c051bfc0f1fc159d59a83f5
RobinM2022 <robin.mbondo@gmail.com>
2022-12-02 10:04:25 +0000

    Ammended the code to fit in with Betty coding style

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
-  * free_listint - Frees a listint_t list.
-  * @head: A pointer to the head of the listint_t list.
-  *
-  * Return: Nothing. 
-  */
+ * free_listint - Frees a listint_t list.
+ * @head: A pointer to the head of the listint_t list.
+ *
+ * Return: Nothing.
+ */
 void free_listint(listint_t *head)
 {
 	listint_t *tmp;
